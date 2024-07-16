9bc628f6391ad39895cd63a46910842d071fe8f9
Carlos Eduardo <eduardosantoos573@gmail.com>
2024-07-16 00:23:43 -0300

    refactor: id table users and removing any functionality
    feat: create table reservations

---
+++ backend/src/database/migrations/20240715223941_reservation.js
@@ -0,0 +1,32 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  const exists = await knex.schema.hasTable("reservations");
+
+  if (!exists){
+    return knex.schema.createTable("reservations", function(table) {
+      table.increments("id");
+      table.primary("id");
+      table.string('phone', 255)
+        .notNullable()
+        .unique()
+        .checkLength('=', 11);
+      table.date('date').notNullable();
+      table.time('time').notNullable();
+      table.uuid('user_id').defaultTo(knex.fn.uuid()).notNullable();
+      table.foreign('user_id').references('users.id').onUpdate("CASCADE").onDelete("RESTRICT");
+      table.timestamp('created_at', {precision: 6}).defaultTo(knex.fn.now(6));
+      table.timestamp('updated_at').defaultTo(knex.raw('NULL ON UPDATE CURRENT_TIMESTAMP'));
+    })
+  }
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable("reservations");
+}
