3ba605d686fc63a8da4d656685b5843c0ef6a5f3
Carlos Eduardo <eduardosantoos573@gmail.com>
2024-07-16 00:24:23 -0300

    feat: create seed reservations

--- backend/src/database/seeds/dev/seed_users.js
+++ backend/src/database/seeds/dev/seed_users.js
@@ -6,6 +6,7 @@ import { crypt } from '../../../controller/utils/cryptography.js';
  */
 export async function seed(knex) {
   // Deletes ALL existing entries
+  await knex("reservations").del();
   await knex('users').del()
 
   let pass = "password";
@@ -15,33 +16,48 @@ export async function seed(knex) {
       first_name: 'Alexander', 
       last_name: 'Frizon',
       email: 'alexander.frizon@example.com',
-      phone: '12345678901',
       role: 'admin',
-      password: crypt(pass),
-      date: '2024-08-01',
-      time: '10:00:00',
+      password: crypt(pass)
     },
     {
       id: uuid(),
       first_name: 'John', 
       last_name: 'Doe',
       email: 'john.doe@example.com',
-      phone: '98765432101',
       role: 'user',
-      password: crypt(pass),
-      date: '2024-08-02',
-      time: '11:00:00', 
+      password: crypt(pass)
     },
     {
       id: uuid(), 
       first_name: 'Jane', 
       last_name: 'Smith',
       email: 'jane.smith@example.com',
-      phone: '09876543211',
       role: 'user',
       password: crypt(pass),
-      date: '2024-08-03',
-      time: '12:00:00',
+    }
+  ]);
+
+  await knex("reservations").insert([
+    {
+      id: 1,
+      user_id: knex("users").select("id").first().where({ email: "alexander.frizon@example.com" }),
+      date: "2024-08-01",
+      time: "10:00:00",
+      phone: "12345678901",
+    },
+    {
+      id: 2,
+      user_id: knex("users").select("id").first().where({ email: "john.doe@example.com" }),
+      date: "2024-08-02",
+      time: "11:00:00",
+      phone: "98765432101",
+    },
+    {
+      id: 3,
+      user_id: knex("users").select("id").first().where({ email: "jane.smith@example.com" }),
+      date: "2024-08-03",
+      time: "12:00:00",
+      phone: "09876543211",
     }
   ]);
 };
