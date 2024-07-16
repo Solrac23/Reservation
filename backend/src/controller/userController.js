c77f421058e92483d889ab6fdad451ff88b8754f
Carlos Eduardo <eduardosantoos573@gmail.com>
2024-07-16 00:21:30 -0300

    feat: adding router create user

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -1,16 +1,18 @@
 import connection from "../database/connection.js";
+import { crypt } from "./utils/cryptography.js";
 import Cookies from "js-cookie";
 import ErrorHandler from "../error/error.js";
 
 export default {
   async getAllUsers(req, res, next) {
     try {
-      const token = Cookies.get("token");
-      console.log(token)
       
       const users = await connection("users").select("*");
 
-      return res.status(200).json({users});
+      return res.status(200).json({
+        success: true,
+        users
+      });
     } catch (error) {
       return next(new ErrorHandler("Erro ao listar usuários", 500));
     }
@@ -20,9 +22,6 @@ export default {
     const { userId } = req;
 
     try {
-      const token = Cookies.get("token");
-      console.log(token)
-      // res.cookie("token")
       const user = await connection("users")
        .where("id", userId)
        .select("*")
@@ -32,10 +31,74 @@ export default {
         return next(new ErrorHandler("Usuário não encontrado", 404));
       }
 
-      return res.status(200).json(user);
+      return res.status(200).json({
+        success: true,
+        user
+      });
     } catch (error) {
       return next(new ErrorHandler("Erro ao buscar usuário", 500));
     }
+  },
+  async createUser(req, res, next) {
+    const { 
+      first_name, 
+      last_name, 
+      email, 
+      role, 
+      password
+  } = req.body;
+
+  let newRole;
+  // Verificando se todos os campos obrigatórios estão preenchidos
+  if (!first_name || !last_name || !email || !password) {
+      // Se algum campo estiver em branco, retorna um erro utilizando o ErrorHandler
+      return next(new ErrorHandler("Por favor, preencha o formulário de reserva completo!!", 400));
+  }
+
+  if(!role) {
+      newRole = "user";
+  }
+
+  const pass = crypt(password)
+
+  const userAlreadyExists = await connection("users")
+      .where("email", email)
+      .select("*")
+      .first()
+  
+  if(userAlreadyExists){
+      return next(new ErrorHandler("User already exists", 409))
+  }
+
+  try {
+      // Tentando criar uma nova reserva no banco de dados utilizando o modelo Reservation
+     const users = await connection('users').insert({
+          first_name, 
+          last_name, 
+          email, 
+          role: newRole,
+          password: pass  // A senha é criptografada antes de ser salva no banco de dados
+      });
+      // Se a criação for bem-sucedida, envia uma resposta de sucesso ao cliente
+      return res.status(201).json({
+          success: true,
+          message: "Reserva enviada com sucesso!",
+      });
+  } catch (error) {
+      // Se ocorrer algum erro durante o processo de criação da reserva
+      console.log(`Error thrown: ${error}`);
+
+      // Verificando se o erro é do tipo ValidationError, que pode ocorrer devido a validações do esquema da reserva
+      if (error.name === 'ValidationError') {
+          // Se for um erro de validação, mapeia os erros individuais e os junta em uma mensagem
+          const validationErrors = Object.values(error.errors).map(err => err.message);
+          // Retorna um erro utilizando o ErrorHandler com a mensagem de validação
+          return next(new ErrorHandler(validationErrors.join(', '), 400));
+      }
+
+      // Se não for um erro de validação, repassa o erro para o próximo middleware de tratamento de erros
+      return next(error);
+  }
   },
 }
 
