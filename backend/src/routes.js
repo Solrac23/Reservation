5186980a118f91e9c9e3fda2b8fccc9ad0243205
Carlos Eduardo <eduardosantoos573@gmail.com>
2024-07-16 00:20:08 -0300

    feat: adding create user and create reservation

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,7 +4,7 @@ import { Router } from "express";
 // Importando a função sendreservation do arquivo de controle reservation.js
 import authMiddleware from "./middleware/authMiddleware.js"
 import roleMiddleware from "./middleware/roleMiddleware.js";
-import { sendreservation } from "./controller/reservation.js";
+import reservationController from "./controller/reservationController.js";
 import userController from "./controller/userController.js"
 import authController from "./controller/authController.js"
 
@@ -13,8 +13,11 @@ const router = Router();
 
 router.get("/user/list", authMiddleware, roleMiddleware, userController.getAllUsers)
 router.get("/user", authMiddleware, userController.getUserById)
+router.post("/user/create", userController.createUser);
+
 router.post("/auth/login", authController.authenticate);
 // Definindo uma rota POST "/send" , que chama a função sendreservation quando a rota é acessada
-router.post("/send", sendreservation);
+router.get("/reservation/:reservationId", authMiddleware, reservationController.getReservationByID)
+router.post("/send", authMiddleware, reservationController.sendReservation);
 
 export default router;
