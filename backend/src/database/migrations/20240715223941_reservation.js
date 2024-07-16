661a9bd97ca9cb302e6c4a53dcacc1b71bc008ed
Carlos Eduardo <eduardosantoos573@gmail.com>
2024-07-16 20:49:10 -0300

    refactor: new column people

--- backend/src/database/migrations/20240715223941_reservation.js
+++ backend/src/database/migrations/20240715223941_reservation.js
@@ -11,14 +11,14 @@ export async function up(knex) {
       table.primary("id");
       table.string('phone', 255)
         .notNullable()
-        .unique()
         .checkLength('=', 11);
       table.date('date').notNullable();
       table.time('time').notNullable();
+      table.integer("people", 255).notNullable();
       table.uuid('user_id').defaultTo(knex.fn.uuid()).notNullable();
       table.foreign('user_id').references('users.id').onUpdate("CASCADE").onDelete("RESTRICT");
       table.timestamp('created_at', {precision: 6}).defaultTo(knex.fn.now(6));
-      table.timestamp('updated_at').defaultTo(knex.raw('NULL ON UPDATE CURRENT_TIMESTAMP'));
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
     })
   }
 }
