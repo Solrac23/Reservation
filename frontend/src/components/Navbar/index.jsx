3b0727915ba36aaa269c3d301ce8a92844ae2b4e
Carlos Eduardo <eduardosantoos573@gmail.com>
2024-07-16 23:29:19 -0300

    refactor: adding register of user and optimize html

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,16 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom"
 import './styles.css'
+import api from "../../services/api";
 import { data } from "../../restApi.json";
+import toast from "react-hot-toast";
 import { Link as Scroll} from "react-scroll";
-import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { FiLogIn } from "react-icons/fi"
 const Navbar = () => {
+	const [user, setUser] = useState("")
 	const [show, setShow] = useState(false);
+	const navigate = useNavigate();
+	useEffect(() => {
+		const token = localStorage.getItem("token");
+    if (token) {
+      setShow(true);
+    } else {
+      setShow(false);
+			navigate("/");
+    }
+	}, [navigate])
+
+	useEffect(() => {
+		async function handleUserLogin(){
+			const token = localStorage.getItem("token");
+
+			try {
+        const response = await api.get("user", {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+				const user = response.data.user
+				setUser(user);
+        console.log("Usuário logado:", user.first_name);
+      } catch (error) {
+        console.error("Falha ao verificar o token", error);
+				localStorage.removeItem('token');
+				localStorage.removeItem('role');
+        toast.error("Sessão expirada. Faça login novamente");
+				setShow(false);
+        return;
+      }
+		}
+
+		handleUserLogin();
+
+	}, [])
 	return (
 			<>
 				<nav>
-					<div className="logo">Alexsandro</div>
+					<div className="logo">{ !user ? "Alexsandro" : user.first_name}</div>
 					<div className={show ? "navLinks showmenu" : "navLinks"}>
 						<div className="links">
 								{data[0].navbarLinks.map((element) => (
@@ -25,20 +62,19 @@ const Navbar = () => {
 										</Scroll>
 								))}
 						</div>
-						<button className="menuBtn">NOSSO MENU</button>
-					</div>
-					<div className="login-container">
-							<div className="login">
-								<Link to={"/login"} href="/login">
-									<FiLogIn size={16} color="#000"/>
-									Login
-									</Link>
-							</div>
-							<div className="signup">
-							<Link to={"/signup"}>Sign Up</Link>
-							</div>
+						<div className="menuBtn">
+							<Scroll
+								to="menu"
+								spy={true}
+								smooth={true}
+								duration={500}
+							>
+								NOSSO MENU
+							</Scroll>
+						</div>
 					</div>
 					<div className="hamburger" onClick={() => setShow(!show)}>
+								casas
 							<GiHamburgerMenu />
 					</div>
 				</nav>
