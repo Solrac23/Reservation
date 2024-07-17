32e464d360fc60058b9a3c60123180184619cac7
Carlos Eduardo <eduardosantoos573@gmail.com>
2024-07-16 23:32:35 -0300

    feat: adding new component Register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import { Toaster } from "react-hot-toast"
 import Home from "./Pages/Home";
 import Success from "./Pages/Success";
 import NotFound from "./Pages/NotFound";
+import { Register } from "./Pages/Register";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/success" element={<Success />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
