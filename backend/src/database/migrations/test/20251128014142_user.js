6ddc248da773dca57138bf0fbca68118062519c9
Carlos Eduardo <eduardosantoos573@gmail.com>
2025-11-27 23:22:23 -0300

    feat: create folder to dev and test

---
+++ backend/src/database/migrations/test/20251128014142_user.js
@@ -0,0 +1,27 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+	return knex.schema.createTable("user", function(table) {
+		table.primary("id").increments();
+		table.string("first_name", 255).notNullable().checkLength(">=", 3).checkLength("<=", 30);
+		table.string("last_name", 255).notNullable().checkLength(">=", 3).checkLength("<=", 30);
+		table.string("email", 255).notNullable().unique();
+		table.enu("role", ["admin", "user"], {
+			enumName: "user_roles"
+		}).defaultTo("user").notNullable();
+		table.string("password").notNullable();
+		table.timestamp("created_at", {precision: 6}).defaultTo(knex.fn.now(6));
+		table.timestamp("updated_at").defaultTo(knex.raw("NULL ON UPDATE CURRENT_TIMESTAMP"));
+		table.unique("id");
+	});
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+	return knex.schema.dropTable("user");
+}
