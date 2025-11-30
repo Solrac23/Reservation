0931369c6ab67e5ea92a2f05315127c99a41e4eb
Carlos Eduardo <eduardosantoos573@gmail.com>
2025-11-30 01:48:14 -0300

    refactor: lint, formatter coding

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,24 +1,23 @@
-// Importando o módulo express, que é um framework web para Node.js
 import { Router } from "express";
 
 // Importando a função sendreservation do arquivo de controle reservation.js
-import authMiddleware from "./middleware/authMiddleware.js"
+import authMiddleware from "./middleware/authMiddleware.js";
 import roleMiddleware from "./middleware/roleMiddleware.js";
 import reservationController from "./controller/reservationController.js";
-import userController from "./controller/userController.js"
-import authController from "./controller/authController.js"
+import userController from "./controller/userController.js";
+import authController from "./controller/authController.js";
 
 // Criando uma nova rota utilizando o método Router do Express
 const router = Router();
 
-router.get("/user/list", authMiddleware, roleMiddleware, userController.getAllUsers)
-router.get("/user", authMiddleware, userController.getUserById)
+router.get("/user/list", authMiddleware, roleMiddleware, userController.getAllUsers);
+router.get("/user", authMiddleware, userController.getUserById);
 router.post("/user/create", userController.createUser);
 
 router.post("/auth/login", authController.authenticate);
 // Definindo uma rota POST "/send" , que chama a função sendreservation quando a rota é acessada
-router.get("/reservations", authMiddleware, reservationController.getAllReservation)
-router.get("/reservation/:reservationId", authMiddleware, reservationController.getReservationByID)
+router.get("/reservations", authMiddleware, reservationController.getAllReservation);
+router.get("/reservation/:reservationId", authMiddleware, reservationController.getReservationByID);
 router.post("/send", authMiddleware, reservationController.sendReservation);
 
 export default router;
