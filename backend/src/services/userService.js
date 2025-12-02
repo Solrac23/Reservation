c4edeac760843f9b512ebcaa6e1b185355b66257
Carlos Eduardo <eduardosantoos573@gmail.com>
2025-12-01 23:25:28 -0300

    feat: new user service layer

---
+++ backend/src/services/userService.js
@@ -0,0 +1,59 @@
+import { crypt } from "../controller/utils/cryptography.js";
+import db from "../database/db.js";
+import ErrorHandler from "../error/error.js";
+
+export async function createUserService({
+	first_name,
+	last_name,
+	email,
+	password,
+	role
+}) {
+
+	// Verificando se todos os campos obrigatórios estão preenchidos
+	if (!first_name || !last_name || !email || !password) {
+		// Se algum campo estiver em branco, retorna um erro utilizando o ErrorHandler
+		throw new ErrorHandler("Por favor, preencha o formulário de reserva completo!!", 400);
+	}
+
+	if(!role) {
+		role = "user";
+	}
+
+	const pass = crypt(password);
+
+	const userAlreadyExists = await db("users")
+		.where("email", email)
+		.select("*")
+		.first();
+  
+	if(userAlreadyExists){
+		throw new ErrorHandler("User already exists", 409);
+	}
+
+	try {
+		// Tentando criar uma nova reserva no banco de dados utilizando o modelo Reservation
+		await db("users").insert({
+			first_name, 
+			last_name, 
+			email, 
+			role,
+			password: pass  // A senha é criptografada antes de ser salva no banco de dados
+		});
+	} catch (err) {
+		// Se ocorrer algum erro durante o processo de criação da reserva
+		throw new ErrorHandler("Erro ao criar usuário", 400);
+	}
+}
+
+export async function findAllUserService() {
+	try {
+		
+		const users = await db.select("first_name, last_name, email, password, role").from("users");
+
+		console.log(users);
+
+	} catch (err) {
+		throw new ErrorHandler("Erro ao listar usuários", 500);
+	}
+}
