eb35595f6171aea1ca06b88cfa290fc786a24a91
Carlos Eduardo <eduardosantoos573@gmail.com>
2025-12-06 02:38:09 -0300

    refactor: implemnt reservation service layer on controller reservation

--- backend/src/controller/reservationController.js
+++ backend/src/controller/reservationController.js
@@ -1,69 +1,57 @@
-// Importando o ErrorHandler de um módulo externo para lidar com erros personalizados
-import connection from "../database/connection.js";
 import { validateDate } from "./utils/date.js";
 import ErrorHandler from "../error/error.js";
+import { createService, findAllReservationService, findReservationByIdService } from "../services/reservationService.js";
 
-// Função assíncrona responsável por lidar com o envio de reservas
 export default {
-	async getAllReservation(req, res, next) {
+	async findAllReservation(req, res, next) {
 		const { userId } = req;
 
 		if(!userId){
-			return next(new ErrorHandler("ID da reserva não fornecido", 400));
+			throw new ErrorHandler("Usuário não encontrado", 404);
 		}
 		
 		try {
 			// Tentando buscar a reserva no banco de dados utilizando o ID fornecido
-			const reservation = await connection('reservations')
-			.where('user_id', userId)
-			.select("phone", "date", "time", "people");
-			
-			if(!reservation){
-				return next(new ErrorHandler("Reserva não encontrada", 404));
-			}
+			const reservation = await findAllReservationService(userId);
 
 			return res.status(200).json({
 				success: true,
-        reservation
-			})
+				reservation,
+			});
 
-		}catch(error){
-			return next(new ErrorHandler("Não foi possível encontrar a reserva com esse ID", 404));
+		}catch(err){
+			return next(err);
 		}
 	},
-	async getReservationByID(req, res, next) {
+	async findReservationById(req, res, next) {
 		// Extraindo o ID da reserva do parâmetro da URL
 		const { reservationId } = req.params;
+		const { userId } = req;
 
+		if(!userId){
+			throw new ErrorHandler("Usuário não encontrado", 404);
+		}
+		
 		if(!reservationId){
-			return next(new ErrorHandler("ID da reserva não fornecido", 400));
+			throw new ErrorHandler("Id da reserva não fornecido", 400);
 		}
 		
 		try {
 			// Tentando buscar a reserva no banco de dados utilizando o ID fornecido
-			const reservation = await connection('reservations')
-			.where('id', reservationId)
-			.first();
-			
-			if(!reservation){
-				return next(new ErrorHandler("Reserva não encontrada", 404));
-			}
-
-			delete reservation.user_id;
+			const reservation = await findReservationByIdService(reservationId, userId);
 
 			return res.status(200).json({
 				success: true,
-        reservation
-			})
+				reservation
+			});
 
-		}catch(error){
-			return next(new ErrorHandler("Não foi possível encontrar a reserva com esse ID", 404));
+		}catch(err){
+			return next(err);
 		}
 	},
 	
-	async sendReservation(req, res, next) {
+	async create(req, res, next) {
 		
-		// Extraindo os dados da reserva do corpo da requisição
 		const {  
 			date, 
 			time, 
@@ -72,6 +60,7 @@ export default {
 		} = req.body;
 		
 		const { userId } = req;
+
 		if(!userId){
 			return next(new ErrorHandler("Usuário não fornecido", 400));
 		}
@@ -85,16 +74,14 @@ export default {
 			return next(new ErrorHandler("A data da reserva não pode ser inferior a data atual", 400));
 		}
 		
-		
 		try {
 			// Tentando criar uma nova reserva no banco de dados utilizando o modelo Reservation
-			await connection('reservations').insert({
-				user_id: userId, 
-				date, 
-				time, 
+			await createService({
+				date,
+				time,
 				phone,
-				people,  // Número de pessoas na reserva
-			});
+				people
+			}, userId);
 			
 			// Se a criação for bem-sucedida, envia uma resposta de sucesso ao cliente
 			return res.status(201).json({
@@ -102,19 +89,7 @@ export default {
 				message: "Reserva enviada com sucesso!",
 			});
 		} catch (error) {
-			// Se ocorrer algum erro durante o processo de criação da reserva
-			console.log(`Error thrown: ${error}`);
-			
-			// Verificando se o erro é do tipo ValidationError, que pode ocorrer devido a validações do esquema da reserva
-			if (error.name === 'ValidationError') {
-				// Se for um erro de validação, mapeia os erros individuais e os junta em uma mensagem
-				const validationErrors = Object.values(error.errors).map(err => err.message);
-				// Retorna um erro utilizando o ErrorHandler com a mensagem de validação
-				return next(new ErrorHandler(validationErrors.join(', '), 400));
-			}
-			
-			// Se não for um erro de validação, repassa o erro para o próximo middleware de tratamento de erros
 			return next(error);
 		}
 	},
-}
+};
