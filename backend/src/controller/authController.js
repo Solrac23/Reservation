362dd80a3af949aa3f07c028b4f1f034de8e61e6
Carlos Eduardo <eduardosantoos573@gmail.com>
2025-12-06 03:08:56 -0300

    refactor: implemnt authservice

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -1,42 +1,41 @@
-import jwt from "jsonwebtoken"
-import connection from "../database/connection.js";
-import { decrypt } from "./utils/cryptography.js"
+import jwt from "jsonwebtoken";
+import { decrypt } from "./utils/cryptography.js";
 import ErrorHandler from "../error/error.js";
+import { authenticateService } from "../services/authService.js";
 
 export default {
-  async authenticate(req, res, next) {
-    const { email, password } = req.body;
+	async authenticate(req, res, next) {
+		const { email, password } = req.body;
 
-    try {
-      const user = await connection("users")
-      .where("email", email)
-      .select("*")
-      .first();
+		try {
+			const user = await authenticateService(email);
+
+			const isValidPassword = decrypt(password, user.password);
  
-     const isValidPassword = decrypt(password, user.password);
+			if (!user || !isValidPassword) {
+				return next(new ErrorHandler("Email ou senha inválidos", 401));
+			}
  
-     if (!user || !isValidPassword) {
-       return next(new ErrorHandler("Email ou senha inválidos", 401));
-     }
- 
-      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+			const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 
-      delete user.password
+			Object.defineProperty(user, "password", {
+				enumerable: false
+			});
 
-      return res.json({ success: true, token, user });
-    } catch (error) {
+			return res.json({ success: true, token, user });
+		} catch (error) {
 
-      console.log(`Error thrown: ${error}`);
+			console.log(`Error thrown: ${error}`);
 
-      if (error.name === 'ValidationError') {
+			if (error.name === "ValidationError") {
           
-          const validationErrors = Object.values(error.errors).map(err => err.message);
+				const validationErrors = Object.values(error.errors).map(err => err.message);
         
-          return next(new ErrorHandler(validationErrors.join(', '), 400));
-      }
+				return next(new ErrorHandler(validationErrors.join(", "), 400));
+			}
 
-      return next(error);
-    }
+			return next(error);
+		}
    
-  }
-}
+	}
+};
