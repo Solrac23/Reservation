4cafbc729455652cc75561ea5f9f390959cbc1c5
Carlos Eduardo <eduardosantoos573@gmail.com>
2025-12-06 02:34:51 -0300

    refactor: add zero on begin of the numbers 1 to 9

--- backend/src/controller/utils/date.js
+++ backend/src/controller/utils/date.js
@@ -1,6 +1,6 @@
 export function validateDate(date){
 	const d = new Date();
-	const today = `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2,"0")}-${d.getDate()}`;
+	const today = `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2,"0")}-${String(d.getDate()).padStart(2, "0")}`;
 
 	const formatDate = date.substr(0, 10);
 
