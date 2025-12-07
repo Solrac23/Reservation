f8667c933799efa294ca9984f22c820f8724dd9a
Carlos Eduardo <eduardosantoos573@gmail.com>
2025-12-07 15:59:01 -0300

    refactor: removing console.log and organize autchController

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -1,6 +1,3 @@
-import jwt from "jsonwebtoken";
-import { decrypt } from "./utils/cryptography.js";
-import ErrorHandler from "../error/error.js";
 import { authenticateService } from "../services/authService.js";
 
 export default {
@@ -8,33 +5,13 @@ export default {
 		const { email, password } = req.body;
 
 		try {
-			const user = await authenticateService(email);
+			const {token} = await authenticateService({email, password});
 
-			const isValidPassword = decrypt(password, user.password);
- 
-			if (!user || !isValidPassword) {
-				return next(new ErrorHandler("Email ou senha inválidos", 401));
-			}
- 
-			const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+			return res.json({ success: true, token });
+		} catch (err) {
+			console.error(`${err}`);
 
-			Object.defineProperty(user, "password", {
-				enumerable: false
-			});
-
-			return res.json({ success: true, token, user });
-		} catch (error) {
-
-			console.log(`Error thrown: ${error}`);
-
-			if (error.name === "ValidationError") {
-          
-				const validationErrors = Object.values(error.errors).map(err => err.message);
-        
-				return next(new ErrorHandler(validationErrors.join(", "), 400));
-			}
-
-			return next(error);
+			return next(err);
 		}
    
 	}
--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -12,7 +12,6 @@ export default {
 				users,
 			});
 		} catch (err) {
-			console.log(err.message);
 			return next(err);
 		}
 	},
@@ -37,7 +36,6 @@ export default {
 				user
 			});
 		} catch (err) {
-			console.log(err.message);
 			return next(err);
 		}
 	},
@@ -70,19 +68,8 @@ export default {
 				success: true,
 				message: "Cadastro realizado com sucesso!",
 			});
-		} catch (error) {
-			// Se ocorrer algum erro durante o processo de criação da reserva
-			console.log(`Error thrown: ${error}`);
-
-			// Verificando se o erro é do tipo ValidationError, que pode ocorrer devido a validações do esquema da reserva
-			if (error.name === "ValidationError") {
-				// Se for um erro de validação, mapeia os erros individuais e os junta em uma mensagem
-				const validationErrors = Object.values(error.errors).map(err => err.message);
-				// Retorna um erro utilizando o ErrorHandler com a mensagem de validação
-				return next(new ErrorHandler(validationErrors.join(", "), 400));
-			}
-
-			return next(error);
+		} catch (err) {
+			return next(err);
 		}
 	},
 };
