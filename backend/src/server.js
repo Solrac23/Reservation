e7a474a6ee097a76dc9e27665c65070dfcd169ca
Carlos Eduardo <eduardosantoos573@gmail.com>
2025-12-12 20:59:22 -0300

    chore: lint and formatter coding

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,6 @@ import app from "./app.js";
 
 // Iniciando o servidor Express e fazendo-o ouvir na porta especificada nas variáveis de ambiente
 app.listen(process.env.PORT, () => {
-    // Registrando uma mensagem no console indicando que o servidor está operando na porta especificada
-    console.log(`Server está operando na porta: ${process.env.PORT}`);
+	// Registrando uma mensagem no console indicando que o servidor está operando na porta especificada
+	console.log(`Server está operando na porta: ${process.env.PORT}`);
 });
